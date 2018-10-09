544a5f3dd3558ac2ddf15e0cdcab22062b2be183
Quang Nguyen K <quangnk2304@gmail.com>
2018-10-09 16:09:08 +0700

    Update UI

---
+++ BTOnline/View/Controller/Search/SearchViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  SearchViewModel.swift
+//  BTOnline
+//
+//  Created by Quang Nguyen K. on 10/5/18.
+//  Copyright © 2018 Asian Tech Co., Ltd. All rights reserved.
+//
+import Foundation
+import MVVM
+
+final class SearchViewModel: ViewModel {
+
+    var data: [String] = ["ab", "aba", "abac", "abaca", "abacas", "abacate", "abacaxi", "abacay", "abaci"]
+}
